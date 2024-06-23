f33bc9b0dcd89466e6b9a68458eab5ff74b81798
jatin <saxena0104jatin@gmail.com>
2024-06-24 03:14:00 +0530

    footer text remove

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -658,7 +658,7 @@
                 </div>
                 <div class="col-lg-4 col-md-6">
                     <h4 class="text-white mb-4">Newsletter</h4>
-                    <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
+                    <!-- <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p> -->
                     <div class="position-relative w-100">
                         <input class="form-control bg-light border-light w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email">
                         <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
